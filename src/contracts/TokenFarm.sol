b6fd4c79c2944a2ea0025f2708733151cc7a7c75
nickguerra <nickguerra@gmail.com>
2021-04-07 06:32:35 -0500

    added balance checks

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -18,8 +18,11 @@ contract TokenFarm {
         daiToken = _daiToken;
     }
 
-    // 1. Stakes Tokens (Deposit)
+    // Stakes Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        // Require amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
+
         // Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -37,9 +40,21 @@ contract TokenFarm {
 
     }
     
-    // 2. Unstake Tokens (Withdraw)
+    // Unstake Tokens (Withdraw)
 
-    // 3. Issuing Tokens
+    // Issuing Tokens
+    function issueToken() public {
+        for (uint i=0; i < stakers.length; i++){
+         address recipient = stakers[i];
+         uint balance = stakingBalance[recipient];
+
+         if(balance > 0){
+            dappToken.transfer(recipient, balance);
+         }
+        
+        }
+    }
+
 
     
 
