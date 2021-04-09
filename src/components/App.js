e56e643a981ed314c0f184323dffe4c901b3bd3f
nickguerra <nickguerra@gmail.com>
2021-04-09 07:43:34 -0500

    added import for web 3

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,19 @@ import './App.css'
 
 class App extends Component {
 
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected.');
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
